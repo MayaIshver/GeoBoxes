718575def6612b2e78c57c48d7d9282a90e68b51
mayasprogramming <157539535+mayasprogramming@users.noreply.github.com>
2024-07-22 22:57:46 +1200

    Add files via upload
    Updated GeoPoint file. With print statements to make sure it is working as expected.

--- GeoPoint.java
+++ GeoPoint.java
@@ -1,23 +1,47 @@
 package swen221Ass1;
 record GeoPoint(double latitude, double longitude) {
 	GeoPoint{
-		assert(-90<latitude<90); //if fails will throw illegal argument expection
-		assert(-180<longitude<180);
+		//includes 90 and -90 as acceptable
+		//if(!(latitude >= -90.0 ||  90.0 <= latitude) || !(longitude >= -180 || longitude < 180)) {
+		//	throw new IllegalArgumentException();
+		//} //if fails will throw illegal argument expection
+		
+		//includes 180 as acceptable but not -180
+		System.out.println("long "+longitude);
+		System.out.println(!(longitude >= -180 && longitude < 180));
+		if(!(longitude >= -180 && longitude < 180) ){
+			throw new IllegalArgumentException();
+		} 
+		System.out.println("lat "+latitude);
+		System.out.println(!(latitude >= -90 && latitude <= 90));
+		
+		if(!(latitude >= -90 && latitude <= 90) ){
+			throw new IllegalArgumentException();
+		} 
+		
 	}
 	public String toString(int precision) {
-		String.format("[latitude: %.df ,longitude %.df", precision,latitude, precision,lonitude);
+		//System.out.print(precision);
+		String print = String.format("[latitude: %."+precision+"f, longitude: %."+precision+"f]", latitude, longitude);
+
+		System.out.println(print);
+		return String.format("[latitude: %."+precision+"f, longitude: %."+precision+"f]", latitude, longitude);
 	}
 	public String toString() {
-		String.format("[latitude: %.8f ,longitude %.8f", latitude, lonitude);
+		String print =String.format("[latitude: %.8f, longitude: %.8f]", latitude, longitude);
+		
+		System.out.println(print);
+		return String.format("[latitude: %.8f, longitude: %.8f]", latitude, longitude);
 	
 	}
 	public GeoPoint average(GeoPoint p) {
 		//point one is this point 
 		//point 2 is p point 
 		//avg longitute - wrong
-		int avglong = (this.longitude + p.longitude)/2
+		double avglong = (this.longitude + p.longitude)/2;
 		//avg latitude 
-		int avglat = (this.latitude + p.latitude)/2
+		double avglat = (this.latitude + p.latitude)/2;
+		return new GeoPoint(avglat, avglong);
 	}
 	
 }
