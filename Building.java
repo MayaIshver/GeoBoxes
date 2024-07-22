239d1cda9e76c4acf59819d48ac82e6cd76dfafe
mayasprogramming <157539535+mayasprogramming@users.noreply.github.com>
2024-07-22 23:06:17 +1200

    Add files via upload
    Updated building file

--- Building.java
+++ Building.java
@@ -2,35 +2,41 @@ package swen221Ass1;
 
 import java.util.Set;
 
-interface Building2{
+interface Building{
 	Set secondaryAddresses();
 	Address primaryAddress();
 	GeoBox boundingBox();
-	static Building2 of(GeoBox b, Address a) {
-		return new simpleBuilding(b,a); 
+	static Building of(GeoBox b, Address a) {
+		if(b == null || a == null) {
+			throw new NullPointerException();
+		}
+		return new simpleBuilding(b,a);
 	}
-	static Building2 of(GeoBox b, Address a, Set<Address> addressSet);
+	static Building of(GeoBox b, Address a, Set<Address> adds) {
+		if(b == null || a == null || adds == null) {
+			throw new NullPointerException();
+		}
+		if(adds.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		Set<Address> secondaryAddresses = Set.copyOf(adds);
+		return new multiBuilding(b,a,secondaryAddresses);
+	}
 	
 	
 	
 }
 
-record simpleBuilding(GeoBox boundingBox, Address primaryAddress) implements Building2 {
-
+record simpleBuilding(GeoBox boundingBox, Address primaryAddress) implements Building {
 	@Override
 	public Set secondaryAddresses() {
 		// TODO Auto-generated method stub
 		return Set.of();
 	}
 
-	
-	
 
-	
-	
-	
 }
 
-record multiBuilding(GeoBox boundingBox, Address primaryAddress, Set<Address> secondaryAddresses) implements Building2 {
+record multiBuilding(GeoBox boundingBox, Address primaryAddress, Set<Address> secondaryAddresses) implements Building {
 	
 }
